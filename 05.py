8bfd303ae4a0b4b8a2cfae752aef480ded38140e
Rafael <94846981+Faelsil@users.noreply.github.com>
2024-01-01 00:09:32 -0300

    first commit

--- 05.py
+++ 05.py
@@ -1,1 +1,13 @@
-##3um professor quer sortear um dos seus quatro alunos para apagar o quadro, faça um programa que ajude ele, lendo o nome deles e escrevendo o nome escolhido
+#um professor quer sortear um dos seus quatro alunos para apagar o quadro, faça um programa que ajude ele, lendo o nome deles e escrevendo o nome escolhido
+import random
+
+nome1 = input('Digite um nome: ')
+nome2 = input('Digite um nome: ')
+nome3 = input('Digite um nome: ')
+nome4 = input('Digite um nome: ')
+
+lista = [nome1, nome2, nome3, nome4]
+
+sorteio = random.choice(lista)
+
+print('O nome sorteado é: {}'.format(sorteio))
--- 06.py
+++ 06.py
@@ -1,1 +1,13 @@
 #o mesmo professor do desafio anterior quer sorttear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada
+import random
+
+nome1 = str(input('Digite um nome: '))
+nome2 = str(input('Digite um nome: '))
+nome3 = str(input('Digite um nome: '))
+nome4 = str(input('Digite um nome: '))
+
+lista = [nome1, nome2, nome3, nome4]
+random.shuffle(lista)
+
+print('A apresentação dos alunos vai ser:')
+print(lista)
