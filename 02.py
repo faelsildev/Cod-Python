bb6e0bca9a88dc0a6e5922bec48e98f57a57988f
Rafael <155026788+faelsildev@users.noreply.github.com>
2024-01-01 21:15:06 -0300

    Delete 02.py

--- 02.py
+++
@@ -1,8 +0,0 @@
-#Crie um programa que leia um numero qualquer pelo teclado e mostre a sua porção intetira.
-#ex: 6.324,a  parte inteira é 6
-import math
-
-n = float(input('Digite um numero: '))
-
-r = math.trunc(n)
-print('O numero digitado foi {}, e a parte inteira desse numero é {}'.format(n,r))
