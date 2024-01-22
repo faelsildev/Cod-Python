c42432e56fb0a358ab37bfdd94b978ebfa8c74c2
Rafael <155026788+faelsildev@users.noreply.github.com>
2024-01-22 14:21:15 -0300

    Delete 02.py

--- 02.py
+++
@@ -1,8 +0,0 @@
-"""Faça um programa que mostre na tela uma contagem regressiva para estouro de fogos de artificio, indo de 10 até 0, com uma pausa de 1s antes deles"""
-from time import sleep
-
-for cont in range(10, -1, -1):
-     print(cont)
-     sleep(2)
-print('BUM! BUM!')
-
